a0ca3f4dc2b064b61a92fe0790f41cb2f445a22e
mohamed emad <mohamed.emad4bubble@gmail.com>
2017-07-31 00:20:06 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import bottle
+
+@bottle.route('/')
+def index():
+    return "<html><script src='xlr8.js' async></script><script src='app.js' async></script><h1>Header</h1><div id='app'><div id='View'><h1>Index Page</h1><a href='/p1' id='p1'>Page 1</a> <a href='/' id='index'>Index</a> <a href='/p2' id='p2'>Page 2</a></div></div><h1>Footer</h1></html>"
+    
+@bottle.route('/p1')
+def p1():
+    return "<html><script src='xlr8.js' async></script><script src='app.js' async></script><h1>Header</h1><div id='app'><div id='View'><h1>Page 2</h1><a href='/p1' id='p1'>Page 1</a> <a href='/' id='index'>Index</a> <a href='/p2' id='p2'>Page 2</a></div></div><h1>Footer</h1></html>"
+    
+@bottle.route('/p2')
+def p2():
+    return "<html><script src='xlr8.js' async></script><script src='app.js' async></script><h1>Header</h1><div id='app'><div id='View'><h1>Page 2</h1><a href='/p1' id='p1'>Page 1</a> <a href='/' id='index'>Index</a> <a href='/p2' id='p2'>Page 2</a></div></div><h1>Footer</h1></html>"
+    
+    
+@bottle.route('/ajax/p1')
+def ap1():
+    return "<h1>Page 3</h1><a href='/p1' id='p1'>Page 1</a> <a href='/' id='index'>Index</a> <a href='/p2' id='p2'>Page 2</a>"
+    
+@bottle.route('/ajax/p2')
+def ap2():
+    return "<h1>Page 2</h1><a href='/p1' id='p1'>Page 1</a> <a href='/' id='index'>Index</a> <a href='/p2' id='p2'>Page 2</a>"
+    
+@bottle.route('/ajax/index')
+def aindex():
+    return "<h1>Index Page</h1><a href='/p1' id='p1'>Page 1</a> <a href='/' id='index'>Index</a> <a href='/p2' id='p2'>Page 2</a>"
+    
+@bottle.route('/<file>')
+def stat(file):
+    return bottle.static_file(filename=file,root='.')
+    
+bottle.run()
---
+++ develop.xlr8.js
@@ -0,0 +1,46 @@
+function _remove(id) {
+    element=document.getElementById(id);
+    if (element==null){console.log('remove element doesn`t exsit');return null}
+    element.parentNode.removeChild(element);
+    element.destroy;
+}
+function _setEvent(id,event){
+    elements=document.querySelectorAll('#'+id);
+    for(element in elements){
+    element=elements[element];
+    if (element===null){console.log('Set event element doesn`t exsit');continue;}
+    element.href='#';
+    element.onclick=event;
+    }
+}
+
+function Change_View(html,title,url){
+    _remove('View');
+    var View = document.createElement('div');
+    View.id = 'View';
+    document.getElementById('app').appendChild(View);
+    View.innerHTML =html;
+    _refresh();
+    history.pushState({url:title},title, url);
+}
+
+function new_page(link_id,event){
+    pages[link_id].push(event);
+}
+
+function _refresh(){
+    keys=Object.keys(pages);
+    for(keynum in keys){
+    _setEvent(keys[keynum],pages[keys[keynum]]);
+    }
+}
+
+function http(settings, callbackfunc) {
+  var xhttp;
+  xhttp=new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {if (this.readyState == 4 && this.status == 200) {callbackfunc(this);}};
+  xhttp.open(settings['method'],settings['url'], true);
+  xhttp.send();
+}
+
+window.onload=function(){_refresh();}
