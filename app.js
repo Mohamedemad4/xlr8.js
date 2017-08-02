5df85e90349d0032a1284f4cab873db4c35e1d54
mohamed emad <mohamed.emad4bubble@gmail.com>
2017-08-02 17:39:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 pages={
-    'p1':function(){
+    '/p1':function(){
         http({'url':'/ajax/p1','method':'GET'},function(xhttp){
             return Change_View(xhttp.responseText,'page 1','/p1');
         })
     },
-    'p2':function(){
+    '/p2':function(){
         http({'url':'/ajax/p2','method':'GET'},function(xhttp){
             return Change_View(xhttp.responseText,'Page 2','/p2');
         })
     },
-    'index':function(){
+    '/':function(){
         http({'url':'/ajax/index','method':'GET'},function(xhttp){
             return Change_View(xhttp.responseText,'Index Page','/');
         })
